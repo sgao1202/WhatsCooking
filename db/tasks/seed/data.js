cecd6b2e2ff7f596a1e41c67d85945af80c146b6
bessonf <bessondev@gmail.com>
2021-05-02 10:15:11 -0400

    bigger seed data

--- db/tasks/seed/data.js
+++ db/tasks/seed/data.js
@@ -96,71 +96,53 @@ module.exports = [
         aboutMe: 'another computer scientist turned chef',
         recipes: [
             {
-                title: 'Blueberry Pies',
-                picture: './img/blueberryPie.jpg',
-                description: 'These better-than-the-bakery blueberry scones are bursting with juicy blueberries',
+                title: '15-MINUTE HONEY GARLIC CHICKEN',
+                picture: './img/honeygarlicchicken.jpg',
+                description: 'quick and easy juicy chicken!',
                 ingredients: [
                     {
-                        name: 'flour',
+                        name: 'teaspoons olive oil (or canola oil)',
                         portion: 2,
-                        units: 'cups'
+                        units: 'teaspoons'
                     },
                     {
-                        name: 'sugar',
-                        portion: 0.5,
-                        units: 'cup'
-                    },
-                    {
-                        name: 'baking powder',
-                        portion: 2.5,
-                        units: 'tsp'
-                    },
-                    {
-                        name: 'ground cinnamon',
-                        portion: 1,
-                        units: 'tsp'
-                    },
-                    {
-                        name: 'salt',
-                        portion: 0.5,
-                        units: 'tsp'
-                    },
-                    {
-                        name: 'butter',
-                        portion: 0.5,
-                        units: 'cup'
-                    },
-                    {
-                        name: 'heavy cream',
-                        portion: 0.5,
-                        units: 'cup'
-                    }, 
-                    {
-                        name: 'eggs',
-                        portion: 1,
-                        units: 'egg'
-                    },
-                    {
-                        name: 'pure vanilla extract',
+                        name: 'boneless, skinless chicken breasts, cut into small cubes (about 1/2 inch)',
                         portion: 1.5,
-                        units: 'tsp'
+                        units: 'pounds'
                     },
                     {
-                        name: 'fresh or frozen blueberries',
+                        name: 'Salt and black pepper',
                         portion: 1,
-                        units: 'cup'
+                        units: 'pinch'
+                    },
+                    {
+                        name: 'honey',
+                        portion: 3,
+                        units: 'tablespoons'
+                    },
+                    {
+                        name: 'low-sodium soy sauce',
+                        portion: 3,
+                        units: 'tablespoons'
+                    },
+                    {
+                        name: 'garlic, minced',
+                        portion: 3,
+                        units: 'cloves'
+                    },
+                    {
+                        name: 'red pepper flakes (optional)',
+                        portion: 0.25,
+                        units: 'teaspoon'
                     }
                 ],
                 procedure: [
-                    'Whisk flour, sugar, baking powder, cinnamon, and salt together in a large bowl. Grate the frozen butter using a box grater. Add it to the flour mixture and combine with a pastry cutter, two forks, or your fingers until the mixture comes together in pea-sized crumbs. See video above for a closer look at the texture. Place in the refrigerator or freezer as you mix the wet ingredients together.',
-                    'Whisk 1/2 cup heavy cream, the egg, and vanilla extract together in a small bowl. Drizzle over the flour mixture, add the blueberries, then mix together until everything appears moistened.',
-                    'Pour onto the counter and, with floured hands, work dough into a ball as best you can. Dough will be sticky. If it’s too sticky, add a little more flour. If it seems too dry, add 1-2 more Tablespoons heavy cream. Press into an 8-inch disc and, with a sharp knife or bench scraper, cut into 8 wedges.',
-                    'Brush scones with remaining heavy cream and for extra crunch, sprinkle with coarse sugar. (You can do this before or after refrigerating in the next step.)',
-                    'Place scones on a plate or lined baking sheet (if your fridge has space!) and refrigerate for at least 15 minutes.',
-                    'Meanwhile, preheat oven to 400°F (204°C).',
-                    'Line a large baking sheet with parchment paper or silicone baking mat. After refrigerating, arrange scones 2-3 inches apart on the prepared baking sheet(s).',
-                    'Bake for 22-25 minutes or until golden brown around the edges and lightly browned on top. Remove from the oven and cool for a few minutes before topping with vanilla icing.',
-                    'Leftover iced or un-iced scones keep well at room temperature for 2 days or in the refrigerator for 5 days.'
+                    'Heat olive oil in a large skillet over medium-high heat.',
+                    'Lightly season the cubed chicken with salt and pepper. (Go easy because the soy sauce has plenty of sodium.)',
+                    'Add the chicken to the skillet and brown on one side, about 3-4 minutes.',
+                    'Meanwhile, make the glaze. Whisk the honey, soy sauce, garlic and red pepper flakes, if using, in a small bowl until well combined.',
+                    'Add the sauce to the pan and toss to coat the chicken pieces. Cook until chicken is cooked through, 4-5 more minutes.',
+                    'Serve with steamed rice and top with green onions, sesame seeds and a squeeze of lime juice, if desired.'
                 ]
             }
         ],
@@ -171,7 +153,146 @@ module.exports = [
         ],
         comments: [
             {
-                comment: 'this is one of my favorites'
+                comment: 'this is so delicious'
+            }
+        ]
+    },
+    {
+        firstName: 'Farid',
+        lastName: 'Jafri',
+        username: 'fjafri',
+        password: '$2a$16$TR0iphtk84k/wLVRaqEYm.npNn4K0esPRfeg9O7nvYw4o4fCCIS3W',
+        profilePicture: './img/none.jpg',
+        aboutMe: 'incredible chef who also enjoy computer science',
+        recipes: [
+            {
+                title: 'quick & easy creamy herb chicken',
+                picture: './img/herbchicken.jpg',
+                description: 'quick and easy creamy herb chicken filled with so much flavour, ready and on your table in 15 minutes? yes!',
+                ingredients: [
+                    {
+                        name: 'chicken breasts (pounded 1/2-inch thin)',
+                        portion: 4,
+                        units: 'breasts'
+                    },
+                    {
+                        name: 'onion powder and garlic powder',
+                        portion: 2,
+                        units: 'teaspoons'
+                    },
+                    {
+                        name: 'fresh chopped parsley',
+                        portion: 2,
+                        units: 'teaspoon'
+                    },
+                    {
+                        name: 'dried thyme and dried rosemary',
+                        portion: 0.5,
+                        units: 'teaspoon'
+                    },
+                    {
+                        name: 'salt and pepper',
+                        portion: 1,
+                        units: 'pinch'
+                    },
+                    {
+                        name: 'garlic, minced',
+                        portion: 4,
+                        units: 'cloves'
+                    },
+                    {
+                        name: 'milk (or half and half)',
+                        portion: 1,
+                        units: 'cup'
+                    }
+                ],
+                procedure: [
+                    'Coat chicken breasts with the onion and garlic powders and herbs. Season generously with salt and pepper.',
+                    'Heat 1 tablespoon of oil a large pan or skillet over medium-high heat and cook chicken breasts until opaque and no longer pink inside (about 5 minutes each side, depending on thickness). Transfer to a plate; set aside.',
+                    'To the same pan or skillet, heat another 2 teaspoons of olive oil and sauté garlic, with parsley, thyme and rosemary, for about 1 minute, or until fragrant.',
+                    'Stir in milk (or cream); season with salt and pepper, to taste.',
+                    'Bring to a boil; add the cornstarch mixture to the centre of the pan, quickly stirring, until sauce has thickened slightly. Reduce heat and simmer gently for a further minute to allow the sauce to thicken more.',
+                    'Return chicken to the skillet. Sprinkle with extra herbs if desired. Serve immediately.'
+                ]
+            }
+        ],
+        ratings: [
+            {
+                rating: 5
+            }
+        ],
+        comments: [
+            {
+                comment: 'this is an easy recipe'
+            }
+        ]
+    },
+    {
+        firstName: 'Albert',
+        lastName: 'Chen',
+        username: 'achen',
+        password: '$2a$16$TR0iphtk84k/wLVRaqEYm.npNn4K0esPRfeg9O7nvYw4o4fCCIS3W',
+        profilePicture: './img/none.jpg',
+        aboutMe: 'an inspiring chef',
+        recipes: [
+            {
+                title: 'Broccoli Pesto Pasta',
+                picture: './img/pestopasta.jpg',
+                description: 'Today, a verdant, bright broccoli pesto pasta that’s as healthy as it is delicious.',
+                ingredients: [
+                    {
+                        name: 'Cooked pasta of your choice, pasta water reserved',
+                        portion: 1,
+                        units: 'lb'
+                    },
+                    {
+                        name: 'Broccoli florets',
+                        portion: 4,
+                        units: 'cups'
+                    },
+                    {
+                        name: 'Basil leaves, lightly packed',
+                        portion: 2,
+                        units: 'cups'
+                    },
+                    {
+                        name: 'Garlic',
+                        portion: 2,
+                        units: 'cloves'
+                    },
+                    {
+                        name: 'Pine nuts',
+                        portion: 0.25,
+                        units: 'cups'
+                    },
+                    {
+                        name: 'Extra virgin olive oil',
+                        portion: 1,
+                        units: 'cup'
+                    },
+                    {
+                        name: 'salt',
+                        portion: 0.5,
+                        units: 'teaspoon'
+                    }
+                ],
+                procedure: [
+                    'In a large pot bring water ** to a rapid boil.',
+                    'Add the broccoli, bring the water back up to a boil for two minutes. Do not drain the water! Immediately transfer broccoli (with a slotted spoon) to a colander place under cold running water to stop further cooking.',
+                    'Bring the water ** back to a boil and add your favorite pasta and cook according to the package (reserve 1 cup of pasta cooking water before draining).',
+                    'In a large blender or food processor, blend together broccoli, basil, garlic, pine nuts, olive oil, salt and Parmigiano-Reggiano until smooth. Slowly pour in 1 cup of reserved pasta water until a nice sauce is formed. If sauce is too thick, add more pasta water one tablespoon at a time.',
+                    'Toss hot pasta with sauce until completely coated. Top with extra Parmesan, pine nuts and chili flakes if desired. Enjoy!'
+                ]
+            }
+        ],
+        ratings: [
+            {
+                rating: 5
+            }
+        ],
+        comments: [
+            {
+                comment: 'this is a fun dish'
             }
         ]
     }
